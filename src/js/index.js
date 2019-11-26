f14272654596e4218238eec7a0145fbb21bd60b3
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2019-11-26 21:08:42 +0100

    fixed legend on click add new data out of nested data

--- src/js/index.js
+++ src/js/index.js
@@ -1,95 +1,153 @@
-import { getData } from './helpers/fetchData.js';
-import { query } from './helpers/query.js';
+import {
+  getData
+} from './helpers/fetchData.js';
+import {
+  query
+} from './helpers/query.js';
 
-import { select, json, tsv, geoPath, geoMercator, zoom, event, scale } from "d3";
+import {
+  select,
+  json,
+  tsv,
+  geoPath,
+  geoMercator,
+  zoom,
+  event,
+  scale
+} from "d3";
 
-import * as d3 from "d3"; 
+import * as d3 from "d3";
 
 import d3Tip from "d3-tip";
 d3.tip = d3Tip;
 
 
 
-import { feature } from 'topojson';
-const weaponTip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.title; });
+import {
+  feature
+} from 'topojson';
+const weaponTip = d3.tip().attr('class', 'd3-tip').html(function (d) {
+  return d.title;
+});
+
+const svgWidth = window.innerWidth;
+const svgHeight = window.innerHeight;
 
 const svg = select('svg')
-      .attr('width', window.innerWidth)
-      .attr('height', window.innerHeight)
-			.call(weaponTip);
-const projection = geoMercator(); 
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+
+  .call(weaponTip);
+const projection = geoMercator();
 const pathGenerator = geoPath().projection(projection);
 const g = svg.append('g');
 const legendLineheight = 18;
 
 
 
-const url ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-38/sparql";
+const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-38/sparql";
 
 
 
 getData(url, query).then(data => {
   // console.log(data.results.bindings)
-  initD3(data)
-  
-  console.log(data)
-	return data;
-}) 
+  // initD3(data)
 
-
-
-function initD3(places){
-
-  
-const cats = places.map((entry) => {
-  return entry.weaponFunction;
-});
-
-                      // console.log(d3)
-
-  
-const colorscale = d3.scaleOrdinal()
-  .domain(cats)
-  .range(d3.schemeCategory10);
-
-g.append('path')
-  .attr('class', 'sphere')
-  .attr('d', pathGenerator({type: 'Sphere'}));
-
+  // console.log(data)
 
   
 
-	
-svg.call(zoom().scaleExtent([0.5, 10]).on('zoom', () => {
-  var transform = event.transform;
-    // console.log(transform)
-    // console.log(event.target)
-  	// console.log(Math.round(event.transform.k))
-    g.attr('transform', event.transform);
-    // g.selectAll('circle')
-    //   .attr('transform', event.transform)
-}));
  
 
-const sidebarWrapper = svg.append('g')
-  .attr("class", "sidebar-wrapper")
-  .style('transform', `translateX(${innerWidth - 400}px`)
-  .attr('height', window.innerHeight)
-  .attr('width', 400);
+  
+  initD3(data)
 
-const sidebarWidth = sidebarWrapper.attr('width');
-const sidebarHeight = sidebarWrapper.attr('height');
+  // createDropdown(weaponGroups);
+
+
+
+  return data;
+})
+
+function createDropdown(data){
+  const dropdown = d3.select('select');
+  
+  // const testArray = data.map(obj => obj.key)
+
+  // testArray
+
+  dropdown.selectAll('option')
+  .data(data)
+  .enter()
+  .append('option')
+  .attr('value', d => d.key)
+    .text(d => d.key)
+}
+
+
+
+
+
+function initD3(places) {
+
+
+  const cats = places.map((entry) => {
+    return entry.weaponFunction;
+  });
+
+  // console.log(d3)
+
+
+  const colorscale = d3.scaleOrdinal()
+    .domain(cats)
+    .range(d3.schemeCategory10);
+
+  g.append('path')
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({
+      type: 'Sphere'
+    }));
+
+
+
+
+
+  svg.call(zoom().scaleExtent([0.5, 40]).on('zoom', () => {
+    var transform = event.transform;
+
+    g.attr('transform', event.transform);
+    let zoomLevel = d3.event.transform.k;
+    g.selectAll('circle').attr('r', (43 - zoomLevel) / 30);
+
+    // console.log((16 - zoomLevel) / 16)
+
+  }));
+
+  const weaponGroups = d3.nest()
+  .key(function(d) { return d.weaponFunction; })
+  .entries(places);
+
+
+function createSidebar(){
+  const sidebarWrapper = svg.append('g')
+    .attr("class", "sidebar-wrapper")
+    .style('transform', `translateX(${innerWidth - 400}px`)
+    .attr('height', window.innerHeight)
+    .attr('width', 400);
+
+  const sidebarWidth = sidebarWrapper.attr('width');
+  const sidebarHeight = sidebarWrapper.attr('height');
 
   const sidebar = sidebarWrapper.append('rect')
     .attr('class', 'sidebar')
     .attr('height', sidebarHeight)
     .attr('width', sidebarWidth);
 
-    const sidebarContent = sidebarWrapper.append('g')
-      .attr('id', 'content')
-      .style('transform', `translate(${30}px, ${40}px`)
-      .attr('height', sidebarHeight -20)
-      .attr('width', sidebarWidth -20);
+  const sidebarContent = sidebarWrapper.append('g')
+    .attr('id', 'content')
+    .style('transform', `translate(${30}px, ${40}px`)
+    .attr('height', sidebarHeight - 20)
+    .attr('width', sidebarWidth - 20);
 
   const title = sidebarContent.append('text')
     .attr('id', 'title')
@@ -100,68 +158,97 @@ const sidebarHeight = sidebarWrapper.attr('height');
     .text('Plaats van geografische herkomst:')
     .style('transform', `translateY(${(sidebarHeight / 3) * 1.5}px)`);
 
-    const img = sidebarContent.append('image')
+  const img = sidebarContent.append('image')
     .attr('class', 'img');
+}
 
-  
-  const legendWrapper = svg.append('g')
-  	.attr("class", "legend-group");
-  
+
+
+    function selectData(category){
+
+      const newArray = weaponGroups.filter(item => item.key == category);
+      console.log(newArray[0].values)
+      drawCircles(newArray[0].values)
+
+      return;
+    
+    }
+
+    // function updateCircles
+
+   function createLegend(){
+
+    const legendWrapper = svg.append('g')
+    .attr("class", "legend-group");
+    // .on('click', console.log(event.target))
+
   legendWrapper.append("rect")
-        .attr("x", 5)
-				.attr("class", "legend-wrapper");
+    .attr("x", 5)
+    .attr("class", "legend-wrapper");
 
-  
+
   const legend = legendWrapper.selectAll(".legend")
-        .data(colorscale.domain())
-        .enter()
-  			.append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; })
+    .data(colorscale.domain())
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr('data-category', d => d)
+    .attr("transform", function (d, i) {
+      return "translate(0," + i * 20 + ")";
+    })
+    .on('click', function(){ 
+      selectData(d3.event.target.parentNode.dataset.category)
+      // console.log(d)
+    })
 
+  legend.append("rect")
+    .attr("x", 185 - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorscale);
 
-        legend.append("rect")
-        .attr("x", 185 - 18)
-        .attr("width", 18)
-        .attr("height", 18)
-        .style("fill", colorscale);
+  legend.append("text")
+    .attr("x", 185 - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .attr("class", "legend-text")
 
-        legend.append("text")
-        .attr("x", 185 - 24)
-        .attr("y", 9)
-        .attr("dy", ".35em")
-  			.attr("class", "legend-text")
+    .text(function (d) {
+      return d;
+    })
 
-        .text(function(d) { return d; });
-  
   legendWrapper
-  .style("transform", "translate(-200px, 203px)");
+    .style("transform", "translate(-200px, 203px)");
   legendWrapper
-  	.transition().duration(2000)
-  		.style("transform", "translate(10px, 203px)");
+    .transition().duration(2000)
+    .style("transform", "translate(10px, 203px)");
+
+  }
+  Promise.all([
+    tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+    json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+  ]).then(([tsvData, topoJSONdata]) => {
+
+    drawMap(tsvData, topoJSONdata)
+    drawCircles(weaponGroups[5].values)
+
+    createSidebar()
+    createLegend();
+
+  });
 
 
-Promise.all([
-  tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
-  json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
-]).then(([tsvData, topoJSONdata]) => {
-  		
-  				
-  		drawCircles(tsvData, topoJSONdata)
-  					
-});
+  function drawMap(tsvData, topoJSONdata) {
+    const countryName = tsvData.reduce((accumulator, row) => {
 
-function drawCircles(tsvData, topoJSONdata){
-  const countryName = tsvData.reduce((accumulator, row) => {
-        
-    accumulator[row.iso_n3] = row.name;
+      accumulator[row.iso_n3] = row.name;
 
       return accumulator;
-  }, {});
+    }, {});
 
 
 
-  const country = feature(topoJSONdata, topoJSONdata.objects.countries);
+    const country = feature(topoJSONdata, topoJSONdata.objects.countries);
     g.selectAll('path')
       .data(country.features)
       .enter()
@@ -171,63 +258,59 @@ function drawCircles(tsvData, topoJSONdata){
     // .append('title')
     // 	.text(d => countryName[d.id]);
 
-g.selectAll('circle')
-  .data(places)
+
+
+
+
+  }
+
+  function drawCircles(data){
+    
+    g.selectAll('circle')
+    .exit().remove()
+      .data( data )
+  
       .enter()
-  .append('g')
-    .attr('id', d => d.title.value)
-  .append("circle")
+      .append('g')
+      .attr('id', d => d.title)
+      .append("circle")
       .attr("r", 1)
-      .attr("transform", function(d) {
+      .attr("transform", function (d) {
 
 
-      return "translate(" + projection([
-        d.long,
-        d.lat
-      ]) + ")";
+        return "translate(" + projection([
+          d.long,
+          d.lat
+        ]) + ")";
       })
-    .style('fill', function(d){
+      .style('fill', function (d) {
 
-      return colorscale(d.weaponFunction)
-    })
-    .on('mouseenter', weaponTip.show)
-    .on('mouseleave', weaponTip.hide)
-    .on('click', function (d) {
-      
-      // console.log(d3.select(this.parentNode))
-      d3.select('.sidebar-wrapper')
-      .select('text')
-      .data(places)
+        return colorscale(d.weaponFunction)
+      })
+      .on('mouseenter', weaponTip.show)
+      .on('mouseleave', weaponTip.hide)
+      .on('click', function (d) {
+
+        // console.log(d3.select(this.parentNode))
+        d3.select('.sidebar-wrapper')
+          .select('text')
+          .data(places)
           .text(d.title);
 
-      d3.select('#heritage')
-        .text(function(){
-          return `Plaats van geografische herkomst: ${ d.land }`;
-        })
+        d3.select('#heritage')
+          .text(function () {
+            return `Plaats van geografische herkomst: ${ d.land }`;
+          })
+
+        d3.select('#content').select('.img')
+          .attr('xlink:href', d.picturePath)
+
+
+        console.log(d.title);
+
+
+      })
       
-      d3.select('.sidebar-wrapper').select('#content').select('.img')
-      .attr('xlink:href', d.picturePath)
-
-
-          console.log(d.title);
-      
-    // .attr('src', d.picturePath)
-
-    // .attr('class', 'object-image')
-    
-  
-    // .attr("transform", function(d) {
-    //     console.log(d)
-
-    //     return "translate(" + projection([
-    //       d.long,
-    //       d.lat
-    //     ]) + ")";
-    // })
-  
-  
-  
-    })
-}
+  }
 
 }
